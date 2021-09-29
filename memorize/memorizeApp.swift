22848ee4dc68fe6e405431b073488caca4f4f7e4
osa212 <78644067+osa212@users.noreply.github.com>
2021-09-29 21:40:48 +0900

    Initial Commit

---
+++ memorize/memorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  memorizeApp.swift
+//  memorize
+//
+//  Created by Sargylana Osipova on 29.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct memorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
